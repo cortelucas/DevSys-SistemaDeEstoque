32b1739d494cd67a4d27e05705277aa4d895605a
Lucas Corte <corte1994.lc@gmail.com>
2021-09-23 08:50:53 -0300

    inicio do projeto

---
+++ src/SistemaDeEstoqueCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaDeEstoqueCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

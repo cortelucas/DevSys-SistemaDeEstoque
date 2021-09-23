62add6166f7b315d9249d5fd37c4ade216a831cd
Lucas Corte <corte1994.lc@gmail.com>
2021-09-23 08:49:33 -0300

    implementação da classe user

---
+++ src/SistemaDeEstoqueCodeFirst/Models/User.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace SistemaDeEstoqueCodeFirst.Models
+{
+  public class User
+  {
+        public User() =>
+            DateCreated = DateTime.Now;
+
+        public User(int iD, string username, string email, string password, DateTime lastUpdate, string recoveryCode, bool active, int role) : this()
+        {
+            ID = iD;
+            Username = username;
+            Email = email;
+            Password = password;
+            LastUpdate = lastUpdate;
+            RecoveryCode = recoveryCode;
+            Active = active;
+            RoleID = role;
+
+        }
+        
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string RecoveryCode { get; set; }
+        public bool Active { get; set; }
+        public int RoleID { get; set; }
+  }
+}
